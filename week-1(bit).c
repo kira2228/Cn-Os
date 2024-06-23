bc57224cc8359c6d574fc44af48ef130ba9b61bd
slothbear90 <157898603+slothbear90@users.noreply.github.com>
2024-06-23 23:52:25 +0530

    Create week-1(bit).c

---
+++ week-1(bit).c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    char input[100];
+    scanf("%s",input);
+    int inputlength=sizeof(input)-1;
+    char output[2*inputlength];
+    int count=0,j=0;
+    for(int i=0;i<inputlength;i++)
+    {
+        if(input[i]=='1')
+        {
+            count++;
+        }
+        else
+        {
+            count=0;
+        }
+        output[j++]=input[i];
+        if(count==5)
+        {
+            output[j++]='0';
+            count=0;
+        }
+    }
+    output[j]='\0';
+    printf("Input: %s\n",input);
+    printf("output after bit stuffing: %s\n",output);
+}
