72cb1567ceaffc8752892cc98f006059a6012a32
slothbear90 <157898603+slothbear90@users.noreply.github.com>
2024-06-23 23:53:42 +0530

    Create week-6.c

---
+++ week-6.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int a[10][10], n;
+
+void adj(int k);
+
+int main() {
+    int i, j, root;
+    printf("Enter no. of nodes: ");
+    scanf("%d", &n);
+    printf("Enter adjacent matrix\n");
+    for(i = 1; i <= n; i++) {
+        for(j = 1; j <= n; j++) {
+            printf("Enter connecting of %d-->%d: ", i, j);
+            scanf("%d", &a[i][j]);
+        }
+    }
+    printf("Enter root node: ");
+    scanf("%d", &root);
+    adj(root);
+    return 0;
+}
+
+void adj(int k) {
+    int i, j;
+    printf("Adjacent nodes of root node %d:\n", k);
+    for(j = 1; j <= n; j++) {
+        if(a[k][j] == 1) {
+            printf("%d\t", j);
+        }
+    }
+    printf("\n");
+    for(i = 1; i <= n; i++) {
+        if(a[k][i] == 0 && i != k) {
+            printf("%d\t", i);
+        }
+    }
+    printf("\n");
+}
