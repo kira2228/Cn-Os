8c2c7b7c581ec8122e9f6bf9b2bff16f35c199da
slothbear90 <157898603+slothbear90@users.noreply.github.com>
2024-06-25 21:21:53 +0530

    Update week-1(bit).c

--- week-1(bit).c
+++ week-1(bit).c
@@ -1,29 +1,23 @@
 #include <stdio.h>
-
-int main() {
+int main(){
     char input[100];
     scanf("%s",input);
-    int inputlength=sizeof(input)-1;
+    int inputlength = sizeof(input)-1;
     char output[2*inputlength];
     int count=0,j=0;
-    for(int i=0;i<inputlength;i++)
-    {
-        if(input[i]=='1')
-        {
+    for(int i=0;i<inputlength;i++){
+        if(input[i]=='1'){
             count++;
-        }
-        else
-        {
+        }else{
             count=0;
         }
         output[j++]=input[i];
-        if(count==5)
-        {
+        if(count==5){
             output[j++]='0';
             count=0;
         }
     }
     output[j]='\0';
-    printf("Input: %s\n",input);
-    printf("output after bit stuffing: %s\n",output);
+    printf("Input : %s\n",input);
+    printf("Output after bit stuffing : %s\n",output);
 }
